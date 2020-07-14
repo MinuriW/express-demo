d917ad60ef67e37126254159dd615d476c480eaa
Min1997 <mpwickx@gmail.com>
2020-07-15 01:57:08 +0530

    route params & ahdnling http get/post requests

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # express-demo
 Sample REST API (Nodejs &amp; Express)
+
+Run by adding command 'nodemon index.js'
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2'},
+    { id: 3, name: 'course3'},
+];
 app.get('/', (req, res) =>{
     res.send('Hello World');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('The course with given ID was not found');
+    res.send(course);
+});
+
+// app.get('/api/posts/:year/:month', (req, res) => {
+//     res.send(req.query); //http://localhost:3000/api/posts/2018/1?sortBy=name
+//     //res.send(req.params); //http://localhost:3000/api/posts/2018/1
+// });
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+      id: courses.length + 1,
+      name: req.body.name
+    };
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
